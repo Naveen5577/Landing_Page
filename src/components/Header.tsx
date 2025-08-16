9845d71e32892263925af5e9940e3f452a4180cc
Naveen5577 <Pnaveenraja1@gmail.com>
2025-08-16 16:16:37 +0530

    Update brand name to 'Made by Naveen' and remove .bolt folder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
           <div className="flex items-center space-x-2">
             <Zap className="h-8 w-8 text-blue-600" />
             <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-              ProductPro
+              Made by Naveen
             </span>
           </div>
 
